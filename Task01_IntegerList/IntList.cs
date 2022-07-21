a644086bf3119c537dbeb7dddb0ba6fd15d37756
Alexander <shagov25@gmail.com>
2022-07-21 15:31:54 +0300

    Добавьте файлы проекта.

---
+++ Task01_IntegerList/IntList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task01_IntegerList
+{
+    internal struct IntList
+    {
+        List<int> _numList;
+        public List<int> NumList { get { return _numList; } private set { _numList = value; } }
+        /// <summary>
+        /// Метод заполнения списка целыми числами в диапазоне min, max
+        /// </summary>
+        /// <param name="listCapasity">Размер списка</param>
+        /// <param name="min">минимально возможное число в списке</param>
+        /// <param name="max">максимально возможное число в списке</param>
+        internal void InitList(int listCapasity = 100, int min = 0, int max = 100)
+        {
+            _numList = new List<int>(listCapasity);
+            Random r = new Random();
+            for(int i = 0; i < NumList.Capacity; i++)
+                NumList.Add(r.Next(min, max));
+            PrintList();
+        }
+        /// <summary>
+        /// Метод вывода списка в консоль
+        /// </summary>
+        private void PrintList()
+        {
+            Console.WriteLine("Значения списка:");
+            for (int i = 0; i < NumList.Count; i++)
+            {
+                if (i % 20 == 0)
+                    Console.WriteLine();
+                Console.Write(NumList[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new String('=', 60));    
+        }
+        /// <summary>
+        /// Метод удаления чисел в заданном диапазоне min, max
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        internal void RemoveNumbers(int min = 25, int max = 50)
+        {
+            if(NumList == null)
+            {
+                Console.WriteLine("Список на заполнен!");
+                return;
+            }
+            int countDels = 0;
+            Console.WriteLine($"Удаляем числа которые > {min} и < {max}");
+            for(int i = 0; i < NumList.Count; i++)
+            {
+                if (NumList[i] > min && NumList[i] < max)
+                {
+                    countDels++;
+                    NumList.RemoveAt(i);
+                }    
+            }
+            Console.WriteLine($"Кол-во удаленных чисел: {countDels}");
+            PrintList();
+        }
+    }
+}
---
+++ Task01_IntegerList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task01_IntegerList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1 - работа со списком:" +
+                "\n\t* Создать лист целых чисел" +
+                "\n\t* Заполнить лист случайными числами в диапазоне от 0 до 100" + 
+                "\n\t* Вывести лист на экран" +
+                "\n\t* Удалить из листа числа больше 25, но меньше 50" +
+                "\n\t* Вывести лист на экран после удаления чисел");
+            IntList list = new IntList();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Для заполнения листа нажмите - 1\nДля удаления из листа чисел диапазона 25 < n < 50 нажмите - 2" +
+                    "\nДля выхода нажмите - Escape");
+                Console.Write("Ваш выбор: ");
+                ConsoleKey key = Console.ReadKey().Key;
+                Console.WriteLine();
+                switch (key)
+                {
+                    case ConsoleKey.D1:
+                        list.InitList();
+                        break;
+                    case ConsoleKey.D2:
+                        list.RemoveNumbers();
+                        break;
+                    case ConsoleKey.Escape:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine($"Для клавиши {key} нет действий!\n{new String('=', 100)}");
+                        break;
+                }
+            }
+        }
+    }
+}

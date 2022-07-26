05644fbb11368cd34efd8d18d653615c07850b27
Alexander <shagov25@gmail.com>
2022-07-26 14:48:41 +0300

    Завершил задачу 3 - Проверка повторов.

---
+++ Task03_CheckingRepetitions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task03_CheckingRepetitions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 3 - Проверка повторов:" +
+               "\n\t* Пользователь вводит число." +
+               "\n\t* Число сохраняется в HashSet коллекцию." +
+               "\n\t* Если такое число уже присутствует в коллекции, то выводится сообщение, что число уже вводилось ранее." +
+               "\n\t* Если числа нет, то появляется сообщение о том, что число успешно сохранено.");
+            Console.WriteLine(new String('=', 120));
+            UniqueSet uniqueSet = new UniqueSet();
+            while (true)
+            {
+                Console.Write("Введите число для проверки на уникальность(для выхода нажмите - Enter): ");
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    uniqueSet.AddUniqueNum(num);
+                }
+                else
+                    break;
+            }
+            uniqueSet.PrintUniqSet();
+        }
+    }
+}
---
+++ Task03_CheckingRepetitions/UniqueSet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task03_CheckingRepetitions
+{
+    /// <summary>
+    /// Структура уникальных значений
+    /// </summary>
+    internal struct UniqueSet
+    {
+        /// <summary>
+        /// Хэшсэт уникальных значений типа int
+        /// </summary>
+        static HashSet<int> _uSet = new HashSet<int> ();
+        public static HashSet<int> USet { get { return _uSet; }}
+        /// <summary>
+        /// Метод добавления значений, типа int, в уникальный список
+        /// </summary>
+        /// <param name="num">Число введеное пользователем</param>
+        internal void AddUniqueNum(int num)
+        {
+            if (USet.Contains(num))
+            {
+                Console.WriteLine($"Невозможно добавить! Число - {num} уже присутствует в списке уникальных чисел!");
+            }
+            else
+            {
+                USet.Add(num);
+                Console.WriteLine($"Число - {num} успешно добавлено, как уникальное!");
+            }
+        }
+        /// <summary>
+        /// Метод вывода уникального списка в консоль
+        /// </summary>
+        internal void PrintUniqSet()
+        {
+            Console.WriteLine("Значения списка уникальных значений: ");
+            foreach(int u in USet)
+                Console.WriteLine("\t" + u);
+        }
+    }
+}
